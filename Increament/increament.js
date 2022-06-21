0f686cb6a5706eff5d7e017a564f8a8ce34c5e13
KingJ500 <106692025+KingJ500@users.noreply.github.com>
2022-06-20 15:26:43 -1100

    increment path
     increment path

---
+++ Increament/increament.js
@@ -0,0 +1,22 @@
+let saveEl = document.getElementById('save-el');
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+console.log(saveEl)
+
+function increment() {
+    count += 1;
+    countEl.innerText = count
+}
+ 
+function save(){
+    let countStr= ' ' + count + ' -' 
+    saveEl.innerText += countStr
+    countEl.innerText = 0;
+    count = 0;
+}
+
+
+
+
+ 
